bd7e8fd9c28e5798355be4b0e4b344ef025ea5c8
MGSOPS\dariiai <Dariia.Iemelianova@derivco.co.uk>
2020-11-18 16:44:16 +0200

    Pact tests.

---
+++ BogApiContentClient.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Bog.Api.Domain.Models.Http;
+
+namespace MyPactTest
+{
+    /// <summary>
+    /// https://github.com/pact-foundation/pact-net
+    /// </summary>
+    public class BogApiContentClient
+    {
+        private const string BOG_API_HOST_URL = "https://bogapi9a005f.azurewebsites.net:443";
+        private readonly HttpClient _client;
+
+        public BogApiContentClient(string baseUri = null)
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(baseUri ?? BOG_API_HOST_URL)
+            };
+        }
+
+        public async Task<ContentResponse> GetArticleContent(string contentId)
+        {
+            contentId ??= "a26ce357-e3c5-4828-2a4b-08d8422b1ed9";
+            string contentRequestUri = $"/api/content/article/{contentId}";
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, contentRequestUri);
+            //httpRequestMessage.Headers.Add("Accept", "application/json");
+
+            var httpResponse = await _client.SendAsync(httpRequestMessage);
+
+            var reasonPhrase = httpResponse.ReasonPhrase;
+
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                throw new Exception(reasonPhrase);
+            }
+
+            var content = await httpResponse.Content.ReadAsStringAsync();
+
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                return null;
+            }
+
+            var contentResponse = JsonSerializer.Deserialize<ContentResponse>(content);
+            return contentResponse;
+        }
+    }
+}
+
+
+
+/*
+             static JsonSerializerOptions Options => new JsonSerializerOptions
+                                                {
+                                                    PropertyNameCaseInsensitive = true,
+                                                    IgnoreNullValues = true,
+                                                    AllowTrailingCommas = true,
+                                                    WriteIndented = true
+                                                };
+             */
---
+++ SomethingApiConsumerTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Mocks.MockHttpService.Models;
+using Xunit;
+
+namespace MyPactTest
+{
+    public class SomethingApiConsumerTests : IClassFixture<ConsumerBogApiPact>
+    {
+        private IMockProviderService _mockProviderService;
+        private string _mockProviderServiceBaseUri;
+
+        public SomethingApiConsumerTests(ConsumerBogApiPact data)
+        {
+            _mockProviderService = data.MockProviderService;
+            _mockProviderService.ClearInteractions(); //NOTE: Clears any previously registered interactions before the test is run
+            _mockProviderServiceBaseUri = data.MockProviderServiceBaseUri;
+        }
+
+        [Fact]
+        public async Task GetSomething_WhenTheTesterSomethingExists_ReturnsTheSomething()
+        {
+            //Arrange
+            _mockProviderService
+                .Given("There is a something with id 'tester'")
+                .UponReceiving("A GET request to retrieve the something")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Get,
+                    Path = "/api/content/article/a26ce357-e3c5-4828-2a4b-08d8422b1ed9",
+                    //Headers = new Dictionary<string, object>
+                    //{
+                    //    { "Accept", "application/json" }
+                    //}
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 200,
+                    Headers = new Dictionary<string, object>
+                    {
+                        { "Content-Type", "application/json; charset=utf-8" }
+                    },
+                    Body = new //NOTE: Note the case sensitivity here, the body will be serialised as per the casing defined
+                    {
+                        Id = "a26ce357-e3c5-4828-2a4b-08d8422b1ed9",
+                        Title =  "This works",
+                        Author = "mrPt",
+                        IsPublished = false
+                    }
+                }); //NOTE: WillRespondWith call must come last as it will register the interaction
+
+            var consumer = new BogApiContentClient(_mockProviderServiceBaseUri);
+
+            //Act
+            var result = await consumer.GetArticleContent("a26ce357-e3c5-4828-2a4b-08d8422b1ed9");
+
+            //Assert
+            Assert.Equal("This works", result.Title);
+
+            _mockProviderService.VerifyInteractions(); //NOTE: Verifies that interactions registered on the mock provider are called at least once
+        }
+    }
+}
